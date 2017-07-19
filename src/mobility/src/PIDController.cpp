a5f329a2a1bcee8a55e70bc87bf01ddf18ff1e5a
jtisaacs <jason.isaacs@csuci.edu>
2017-07-18 18:30:59 -0700

    Updating PID controller with fixes from Dan's suggestions.

--- src/mobility/src/PIDController.cpp
+++ src/mobility/src/PIDController.cpp
@@ -4,15 +4,11 @@
 #include <math.h>
 #include <angles/angles.h>
 
-PIDController::PIDController()
-{
-
-}
-
 float PIDController::calculateVelocity(geometry_msgs::Pose2D currentLocation, geometry_msgs::Pose2D goalLocation)
 {
     float current_error = pid_error.calculateCurrentError(currentLocation, goalLocation);
     float error_derivative = pid_error.calculateDerivative(current_error);
+    this->updateErrorIntegrator(goal_location,current_error);
     float error_integral = pid_error.getIntegrator();
     float desired_velocity = (gains.KP*current_error) + (gains.KI*error_integral) + (gains.KD*error_derivative);
     desired_velocity = Saturation::saturation(desired_velocity, MAX_VELOCITY);
